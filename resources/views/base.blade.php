c2ef2e917ac91714559281c4cf56fc4076d7e82c
optimum-02 <mawou.aa@gmail.com>
2024-01-11 18:23:34 +0100

    scafold to avoid repetition

---
+++ resources/views/base.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Hello City</title>
+
+
+
+
+</head>
+@yield('content')
+
+<footer>
+    <p>&copy; Copyright {{date('Y')}} &middot; <a href="/about-us">About us</a></p>
+</footer>
+</body>
+
+</html>
--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,22 +1,6 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>About Us | Hello City</title>
-
-
-
-
-</head>
+@extends('base')
+@section('content')
 <h1>Built with &hearts; by Optimum.</h1>
 <p><a href="/">Revenir à la page d'accueil</a></p>
 
-<footer>
-    <p>&copy; Copyright {{date('Y')}} &middot; </p>
-</footer>
-</body>
-
-</html>
+@endsection
