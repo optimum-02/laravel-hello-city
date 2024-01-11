ceba3f8128583fa7e9c13f3b0103cb98a61acae3
optimum-02 <mawou.aa@gmail.com>
2024-01-11 17:54:12 +0100

    add about page an update link

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,17 +5,17 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Hello City</title>
+    <title>About Us | Hello City</title>
 
 
 
 
 </head>
-<h1>Hello from Cotonou</h1>
-<p>It's currently {{date('h:i A')}}</p>
+<h1>Built with &hearts; by Optimum.</h1>
+<p><a href="/">Revenir à la page d'accueil</a></p>
 
 <footer>
-    <p>&copy; Copyright {{date('Y')}} &middot; <a href="/about-us">About us</a></p>
+    <p>&copy; Copyright {{date('Y')}} &middot; <a href="/">Home</a></p>
 </footer>
 </body>
 
