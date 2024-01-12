374bb3882dd16449aa4acef532573cb95b2fa4d6
optimum-02 <mawou.aa@gmail.com>
2024-01-12 12:58:55 +0100

    add image to page

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -3,6 +3,8 @@
 About Us | {{config('app.name')}}
 @endsection
 @section('content')
+<img src="/images/logo.jpeg" alt="Image cotonou">
+
 <h1>Built with &hearts; by Optimum.</h1>
 <p><a href="{{route('home')}}">Revenir à la page d'accueil</a></p>
 
--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -1,5 +1,6 @@
 @extends('base')
 @section('content')
+<img src="/images/logo.jpeg" alt="Image cotonou">
 <h1>Hello from Cotonou</h1>
 <p>It's currently {{date('h:i A')}}</p>
 @endsection
